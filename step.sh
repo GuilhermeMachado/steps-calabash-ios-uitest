1e873d6226758f34ab776482ee10182d82ba148f
Guilherme Machado <guilherme.menon.machado@gmail.com>
2016-09-22 15:24:24 -0300

    Update step.sh

--- step.sh
+++ step.sh
@@ -7,8 +7,6 @@ set -e
 export BUNDLE_GEMFILE="${this_script_dir}/Gemfile"
 
 bundle update
-bundle exec calabash-ios download
-bundle exec calabash-ios gen
 bundle exec ruby "$this_script_dir/step.rb" \
 	-b "${simulator_device}" \
 	-c "${simulator_os_version}"
